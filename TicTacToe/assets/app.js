38deca27ad0035d4829c00c55e134b9b1871176a
Jonas Kazlauskas <netreneris@gmail.com>
2023-12-14 13:00:13 +0200

    set overlay/modal behavior

--- TicTacToe/assets/app.js
+++ TicTacToe/assets/app.js
@@ -1,2 +1,18 @@
-const firstPlayer = document.getElementById("edit-player-1-btn");
-const secondPlayer = document.getElementById("edit-player-2-btn");
+/* ====================== player name box ====================== */
+const EditPlayer1NameBtn = document.getElementById("edit-player-1-btn");
+const EditPlayer2NameBtn = document.getElementById("edit-player-2-btn");
+
+/* ====================== overlay ====================== */
+const playerConfigOverlay = document.getElementById("config-overlay");
+const backdropElement = document.getElementById("backdrop");
+const cancelConfigOverlayBtn = document.getElementById("cancel-config-button");
+const formNameElement = document.querySelector("form");
+
+/* ====================== player name box ====================== */
+EditPlayer1NameBtn.addEventListener("click", openPlayerConfig);
+EditPlayer2NameBtn.addEventListener("click", openPlayerConfig);
+
+/* ====================== overlay  ====================== */
+cancelConfigOverlayBtn.addEventListener("click", closePlayerConfig);
+backdropElement.addEventListener("click", closePlayerConfig);
+formNameElement.addEventListener("submit", savePlayerConfig);
---
+++ TicTacToe/assets/config.js
@@ -0,0 +1,13 @@
+function openPlayerConfig() {
+  playerConfigOverlay.style.display = "block";
+  backdropElement.style.display = "block";
+}
+
+function closePlayerConfig() {
+  playerConfigOverlay.style.display = "none";
+  backdropElement.style.display = "none";
+}
+
+function savePlayerConfig(event) {
+  event.preventDefault();
+}
